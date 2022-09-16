2fc97af7b907058587b195c5b2a1de3ccd906de1
anaisabelveg <anaisabelveg@gmail.com>
2022-09-16 14:52:04 +0200

    Add files via upload

---
+++ Vue/ejemplo2-productos/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <CatalogoProductos />   
+  </div>
+</template>
+
+<script>
+import CatalogoProductos from '@/components/CatalogoProductos.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    CatalogoProductos,
+  }
+}
+</script>
+
+<style>
+  /* #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  } */
+</style>
---
+++ Vue/ejemplo2-productos/src/components/FormularioAlta.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <h2>Alta de productos</h2>
+        <form @submit.prevent="enviar">
+            <label>ID</label>
+            <input type="text" v-model="producto.id" /> <br/>
+
+            <label>Descripcion</label>
+            <input type="text" v-model="producto.descripcion" /> <br/>
+
+            <label>Precio</label>
+            <input type="text" v-model="producto.precio" /> <br/>
+
+            <button type="submit">Enviar</button>
+            
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FormularioAlta',
+        data() {
+            return {
+                producto: {
+                    id: 0,
+                    descripcion: '',
+                    precio: 0
+                }
+            }
+        },
+        methods:{
+            enviar(){
+                //console.log(this.producto);
+                this.$emit('enviar-producto' , this.producto);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
