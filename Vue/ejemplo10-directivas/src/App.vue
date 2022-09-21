aed88d83dea611345e0c1b44a6bdd75859b1c996
anaisabelveg <anaisabelveg@gmail.com>
2022-09-21 17:59:38 +0200

    Add files via upload

---
+++ Vue/ejemplo10-directivas/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <EjemploDirectivas/>
+</template>
+
+<script>
+import EjemploDirectivas from './components/EjemploDirectivas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EjemploDirectivas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/ejemplo9-render-condicional/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Ejemplo de render condicional</h1>
+
+    <!-- Primera forma -->
+    <!-- <p v-if="logado">El usuario esta logado</p>
+    <p v-if="!logado">El usuario no esta logado</p> -->
+
+    <!-- Segunda forma -->
+    <!-- <p v-if="logado">El usuario esta logado</p>
+    <p v-else>El usuario no esta logado</p> -->
+
+    <p v-if="edad < 18">Lo siento,eres menor de edad</p>
+    <p v-else-if="logado">Eres mayor y estas logado</p>
+    <p v-else>No estas logado</p>
+
+    <!-- v-if es lazy; si de inicio la condicion es false, no aparece en el DOM
+         v-show es eager; aunque la condicion de inicio sea true o false
+                existe en el DOM pero esta oculto -->
+    <p v-show="logado">estas logado con show</p>
+  </div>
+</template>
+
+<script setup>
+
+  const edad = 19
+  const logado = false
+
+</script>
