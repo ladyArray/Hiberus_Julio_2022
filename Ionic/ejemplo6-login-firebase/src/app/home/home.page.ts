1050a03181f41a214127e193b37a6806a2677d3e
anaisabelveg <anaisabelveg@gmail.com>
2022-09-28 09:25:37 +0200

    Add files via upload

--- Ionic/ejemplo6-login-firebase/src/app/home/home.page.ts
+++ Ionic/ejemplo6-login-firebase/src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { LoginService } from '../services/login.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,7 +11,33 @@ export class HomePage {
   registro: boolean = false;   // registro=true modo registro; registro=false modo login
   email:string = '';
   pw: string = '';
+  conf_pw: string = '';
 
-  constructor() {}
+  constructor(private loginService: LoginService) {}
+
+  login(){
+    this.loginService.login(this.email, this.pw)
+        .then(()=> {
+          alert("Usuario logado")
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+  }
+
+  registrar(){
+    if (this.pw == this.conf_pw){
+      this.loginService.registro(this.email, this.pw)
+        .then(()=> {
+          alert("Usuario registrado")
+          this.registro = false;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    } else {
+      alert("Los password no coinciden");
+    }
+  }
 
 }
