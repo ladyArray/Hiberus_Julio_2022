ae7759bfca6749727729c1e26493f273b7999c12
anaisabelveg <anaisabelveg@gmail.com>
2022-09-28 14:44:58 +0200

    Add files via upload

--- Ionic/ejemplo6-login-firebase/src/app/home/home.page.ts
+++ Ionic/ejemplo6-login-firebase/src/app/home/home.page.ts
@@ -1,24 +1,44 @@
 import { LoginService } from '../services/login.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   registro: boolean = false;   // registro=true modo registro; registro=false modo login
   email:string = '';
   pw: string = '';
   conf_pw: string = '';
+  usuario: string = null;
 
   constructor(private loginService: LoginService) {}
+
+  salir(){
+    this.loginService.logout()
+      .then(()=>{
+        this.usuario = null;
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+  }
+
+  usuarioLogado(){
+    this.loginService.comprobarLogado().subscribe((info) => {
+      if (info != null){
+        this.usuario = info.email;
+      }
+    });
+  }
 
   login(){
     this.loginService.login(this.email, this.pw)
         .then(()=> {
           alert("Usuario logado")
+          this.usuarioLogado();
         })
         .catch((error) => {
           console.log(error);
@@ -38,6 +58,10 @@ export class HomePage {
     } else {
       alert("Los password no coinciden");
     }
+  }
+
+  ngOnInit(): void {
+      this.usuarioLogado();
   }
 
 }
